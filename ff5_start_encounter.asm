0bdc0e561270a14553de1711305d5abad8022c96
cubear <Cubed1@gmail.com>
2025-07-20 13:56:37 -0700

    Add files via upload

--- ff5_start_encounter.asm
+++ ff5_start_encounter.asm
@@ -3,15 +3,31 @@
 
 hirom
 
+!steptaken = $0B56
+!e_percent = $16A8
+
+;everyframe:
+;set b56 to 1
+
+
+
 org $C0CABF ;dungeon
- jsl force_encounter;sta $16a8 
+ jsl force_encounter;sta $16A8
  nop ;lda $06
 
 org $C0CBC8 ;world map
  jml force_encounter_world ;cmp $16a9
-							;bcs c0cc20
-							
-org $FAF220
+						;bcs c0cc20
+
+org $c05e96 ;ldy $06
+ jsl set56  ;lda $3f
+
+
+
+
+	
+org $FAF280
+
 force_encounter:
  pha
  lda $02 ;start_button_pressed
@@ -31,7 +47,7 @@ start_not_pressed:
 
 
 done:
- sta $16A8
+ sta !e_percent
  lda $06
  rtl
 
@@ -55,4 +71,19 @@ start_not_pressed_world:
 
 ;else
  jml $C0CC20
+ 
+set56: 
+ rep #$20
+ lda $02 ;start_button_pressed
+ AND #$1000 
+ beq + ;start not pressed
+ sep #$20
+ lda $ba
+ bne +
+ inc $56 ;set step done
++
+ sep #$20
+ ldy $06
+ lda $3f
+ rtl
 ;eof
